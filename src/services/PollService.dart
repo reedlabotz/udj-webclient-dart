91545bd70c5ba50e525e9df2b147244ca802a34a
jtfairbank <jtfairbank@gmail.com>
2012-11-22 00:45:38 -0600

    Add comments to PollService

--- src/services/PollService.dart
+++ src/services/PollService.dart
@@ -1,20 +1,40 @@
 part of udjlib;
 
+// PollService
+// ============================================================================
 
+/**
+ * Service that will get updates from the server.
+ */
 class PollService {
   final UdjApp _udjApp;
   
   Timer _timer;
   
-   PollService(this._udjApp){
+  // Constructor
+  // --------------------------------------------------------------------------
+  
+  PollService(this._udjApp){
     
   }
-   
+  
+  // Methods
+  // --------------------------------------------------------------------------
+  
+  /**
+   * Starts polling the server.
+   * 
+   * UdjApp.pollPlayer must have a Timer paramater since Timer.repeating
+   * requires a callback with that parameter.
+   */
   void start(){
     new Timer.repeating(Constants.POLL_INTERVAL,_udjApp.pollPlayer);
     _udjApp.pollPlayer(null);
   }
   
+  /**
+   * Stops polling the server.
+   */
   void stop(){
     _timer.cancel();
   }
